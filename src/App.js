3133a05ecd91de4a63a71b43bd0a08f545b559f4
andrewkyiv2000 <videodesign2014@gmail.com>
2022-05-26 19:12:35 +0300

    getting data from server using fetch API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((result) =>
+        this.setState(
+          () => {
+            return { isLoaded: true, monsters: result };
+          },
+          () => {
+            console.log(this.state);
+          },
+          (error) => {
+            this.setState({ isLoaded: true, error });
+          }
+        )
+      );
+  }
+
+  render() {
+    const monsters = this.state.monsters;
+    return (
+      <div className="App">
+        {monsters.map((item) => {
+          return <h1 key={item.id}>{item.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
